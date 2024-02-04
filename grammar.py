1c30a4e4a299ce11066fb63ccb627126685edd4c
dylangotlearning <dylan@gotlearning.com>
2024-02-04 11:22:24 -0800

    Solutions and tests passing

--- grammar.py
+++ grammar.py
@@ -4,3 +4,15 @@
 # For example, capitalize("hello world") should return "Hello World"
 # If the first letter of a word is already capitalized, the function should leave that word unchanged
 # Run python3 -m unittest test_functions.py in your terminal to check your work.
+
+def capitalize(text):
+    new = ""
+    previous=None
+    for letter in text:
+        if previous is None or previous == " ":
+            new = new + letter.upper()
+        else:
+            new = new + letter
+        previous = letter
+
+    return new
