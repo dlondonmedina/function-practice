dca6443abfb806db95a16a48e6b6f82243d470c5
dylangotlearning <dylan@gotlearning.com>
2024-02-04 11:22:24 -0800

    Solutions and tests passing

--- calculator.py
+++ calculator.py
@@ -4,3 +4,15 @@
 # For example, calculator(5, 3, "-") should return 3 and not -2.
 # If the string is not one of the specified operations, the function should print "Invalid Operator".
 # Run python3 -m unittest test_functions.py in your terminal to check your work.
+
+def calculator(x, y, operator):
+    if operator == "+":
+        return x + y
+    if operator == "-":
+        return x - y
+    if operator == "*":
+        return x * y
+    if operator == "/":
+        return x / y
+    print("Invalid Operator")
+    return
